fe6711e20caf2880557d35ba5cacf85e8d2fe511
Caio Rosa <caio.cjcr@gmail.com>
2021-08-12 18:41:59 -0300

    test: ensure Login inputs will load with their initial states

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -16,4 +16,13 @@ describe('Login Component', () => {
     const submitButton = getByTestId('submitBtn') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
   })
+  test('Should render inputs with their initial states', () => {
+    const { getByTestId } = render(<Login />)
+    const emailStatus = getByTestId('email-status')
+    const passwordStatus = getByTestId('password-status')
+    expect(emailStatus.title).toBe('Required field')
+    expect(emailStatus.textContent).toBe('🔴')
+    expect(passwordStatus.title).toBe('Required field')
+    expect(passwordStatus.textContent).toBe('🔴')
+  })
 })
