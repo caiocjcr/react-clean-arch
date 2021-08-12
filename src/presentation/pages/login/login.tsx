f3c8acac7873cbb8f1bde56411eb4b2e8e6ce957
Caio Rosa <caio.cjcr@gmail.com>
2021-08-12 15:03:17 -0300

    refactor: break form status into separate component

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Styles from './login-styles.scss'
-import Spinner from '@/presentation/components/spinner/spinner'
 import Header from '@/presentation/components/header'
 import Footer from '@/presentation/components/footer'
 import TextInput from '@/presentation/components/text-input'
+import FormStatus from '@/presentation/components/form-status'
 
 const Login: React.FC = () => {
   return (
@@ -14,10 +14,7 @@ const Login: React.FC = () => {
         <TextInput type="email" name="email" placeholder="Enter your email" />
         <TextInput type="password" name="password" placeholder="Enter your password" />
         <button className={Styles.submit} type="submit">Log me in!</button>
-        <div className={Styles.errorWrap}>
-          <Spinner className={Styles.spinner} />
-          <span className={Styles.error}>Error</span>
-        </div>
+        <FormStatus />
       </form>
       <Footer />
     </div>
