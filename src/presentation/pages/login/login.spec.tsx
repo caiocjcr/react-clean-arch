0965df43f3de583446ffdcd4151f199c74cc91a8
Caio Rosa <caio.cjcr@gmail.com>
2021-08-12 19:04:41 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,25 +1,34 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Login from './login'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+  return { sut }
+}
 
 describe('Login Component', () => {
   test('Should render Login', () => {
     render(<Login />)
   })
   test('Should not render FormStatus children at Login component mount', () => {
-    const { getByTestId } = render(<Login />)
-    const errorWrap = getByTestId('error-wrap')
+    const { sut } = makeSut()
+    const errorWrap = sut.getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
   })
   test('Should render submit button disabled by default', () => {
-    const { getByTestId } = render(<Login />)
-    const submitButton = getByTestId('submitBtn') as HTMLButtonElement
+    const { sut } = makeSut()
+    const submitButton = sut.getByTestId('submitBtn') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
   })
   test('Should render inputs with their initial states', () => {
-    const { getByTestId } = render(<Login />)
-    const emailStatus = getByTestId('email-status')
-    const passwordStatus = getByTestId('password-status')
+    const { sut } = makeSut()
+    const emailStatus = sut.getByTestId('email-status')
+    const passwordStatus = sut.getByTestId('password-status')
     expect(emailStatus.title).toBe('Required field')
     expect(emailStatus.textContent).toBe('🔴')
     expect(passwordStatus.title).toBe('Required field')
