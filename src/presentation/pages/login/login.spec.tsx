b21021987c44ad1865cf531632d0331ea85304d3
Caio Rosa <caio.cjcr@gmail.com>
2021-08-17 13:48:44 -0300

    test: prevent Login from submitting form if its invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -145,10 +145,19 @@ describe('Login Component', () => {
       password
     })
   })
+
   test('Should call Authentication only once', () => {
     const { sut, authenticationSpy } = makeSut()
     simulateValidSubmit(sut)
     simulateValidSubmit(sut)
     expect(authenticationSpy.callsCount).toBe(1)
+  })
+
+  test('Should not call Authentication if form is invalid', () => {
+    const validationError = faker.random.words()
+    const { sut, authenticationSpy } = makeSut({ validationError })
+    populateEmailField(sut)
+    fireEvent.submit(sut.getByTestId('login-form'))
+    expect(authenticationSpy.callsCount).toBe(0)
   })
 })
